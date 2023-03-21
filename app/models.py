36a75186b158f75ca4952ac5b059a139a210817d
Souvik Karmakar <SouvikGeek>
2023-03-22 00:42:00 +0530

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Form(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    date = models.DateField()
+    occupation = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    
---
+++ app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            date = form.cleaned_data['date']
+            occupation = form.cleaned_data['occupation']
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date, occupation=occupation)
+            
+            message_body = f"Thank you for your submission, {first_name}. \n " \
+                                f"Here are your data: \n {first_name} {last_name} \n"  \
+                                    f"Your occupation: {occupation} \n " \
+                                     f"Your joining date is: {date} \n " \
+                                        f"Thank you!"
+            
+            email_message = EmailMessage("Form submission confirmation!", message_body, to=[email])  
+            email_message.send()                          
+            
+            messages.success(request, "Form submitted successfully!")
+                             
+    return render(request, 'index.html')
+
+
+
+def about(request):
+    return render(request, 'about.html')
